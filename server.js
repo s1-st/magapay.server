53fa6fb8ac909f92428280827ad95968db50ecbb
s1-st <mwathageoffrey0@gmail.com>
2026-06-07 11:06:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -185,7 +185,9 @@ app.post("/stkpush", async (req, res) => {
 ========================= */
 app.post("/stk-callback", async (req, res) => {
   try {
-    console.log("FULL WEBHOOK:", JSON.stringify(req.body, null, 2));
+      console.log("RAW BODY:", req.body);
+      console.log("KEYS:", object.keys(req.body));
+console.log("FULL WEBHOOK:", JSON.stringify(req.body, null, 2));
 
     const data = req.body;
 
